9df3b276c6a4e8a905eed938452d2c336c2d5782
eugene <eugene@Deniss-MacBook-Pro.local>
2018-06-17 14:49:45 +0300

    initial commit

---
+++ app/src/main/java/com/example/twitter/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.twitter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.twitter.twitterapplication.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
